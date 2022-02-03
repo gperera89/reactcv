b79a85f6badb6c47ebe6289e1684963862fc6075
gperera89 <80236223+gperera89@users.noreply.github.com>
2022-02-03 11:17:56 +0800

    Update PD.js
    edit wording and and removed h4 tages

--- src/PD.js
+++ src/PD.js
@@ -4,30 +4,30 @@ import Typography from '@mui/material/Typography';
 const PD = () => {
   return (
     <Typography paragraph>
-        <h4>Biology:</h4>
+        <strong>Biology:</strong>
         <ul>
-            <li>Completed Biology Category 2 Diploma Programme Certification with the IBO in May 2020.</li>
-            <li>Completed Category 3 training in Biology: A focus on internal assessment with the IBO in March 2021.</li>
-            <li>Completed Category 3 training in The role of the supervisor in extended essays with the IBO in March 2021.</li>
+            <li>Biology Category 2 Diploma Programme Certification with the IBO in May 2020.</li>
+            <li>Category 3 training in Biology: A focus on internal assessment with the IBO in March 2021.</li>
+            <li>Category 3 training in The role of the supervisor in extended essays with the IBO in March 2021.</li>
+        </ul>   
+        <br />
+        <strong>Chemistry:</strong>
+        <ul>
+            <li>Online course to become an Assistant Examiner with Cambridge International Education (CIE) for the IGCSE Chemistry (0620) Course in July 2019</li>
+            <li>Extension Cambridge IGCSE Chemistry course by Cambridge Assessment International Education in November 2018.</li>
         </ul>
-
-        <h4>Chemistry:</h4>
+        <br />  
+        <strong>Lower Secondary Science:</strong>
         <ul>
-            <li>Completed online course to become an Assistant Examiner with Cambridge International Education (CIE) for the iGCSE Chemistry (0620) Course in July 2019</li>
-            <li>Completed an online course in Extension Cambridge IGCSE Chemistry course by Cambridge Assessment International Education in November 2018.</li>
+            <li>Worksop on producing and implementing rigorous Project Based Learning experiences in 2018.</li>
+            <li>Workshop on producing Service-Learning projects in 2021.</li>
         </ul>
-
-        <h4>Lower Secondary Science:</h4>
+        <br />
+        <strong>Life-Long Learning:</strong>
         <ul>
-            <li>Completed a course on producing an implementing rigorous Project Based Learning experiences in 2018.</li>
-            <li>Completed a workshop on producing Service-Learning projects and opportunities within the curriculum and co-curriculum in 2021.</li>
-        </ul>
-
-        <h4>Life-Long Learning:</h4>
-        <ul>
-            <li>Completed a course to be an Award Leader, Supervisor and Assessor for the Duke of Edinburgh International Award program.</li>
-            <li>Completed Level Two Safeguarding and Child Protection Training by 112 Safeguarding in January 2022.</li>
-            <li>Completed First Aid with CPR training in August 2021 with the American Heart Association in Shanghai.</li>
+            <li>Award Leader, Supervisor and Assessor for the Duke of Edinburgh International Award program.</li>
+            <li>Level Two Safeguarding and Child Protection Training by 112 Safeguarding in January 2022.</li>
+            <li>First Aid with CPR training in August 2021 with the American Heart Association in Shanghai.</li>
             <li>Currently completing an online course on Codecademy on Full-Stack Engineering in Web Development.</li>
         </ul>
     </Typography>
